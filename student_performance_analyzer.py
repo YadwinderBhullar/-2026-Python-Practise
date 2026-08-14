650408cb39f7828b54ef1dda1e2022e1b31c1d73
Yadwinder Singh Bhullar <ysbhullar31@gmail.com>
2026-08-14 17:57:05 -0400

    complete student preformance analyzer

--- student_performance_analyzer.py
+++ student_performance_analyzer.py
@@ -11,12 +11,70 @@ students = {
     "Sarah": [90, 95, 88],
     "David": [35, 42, 48]
 }
-def each_students_average_marks(students):
+def each_students_average_marks(marks):
 
-    for name ,values in students.items():
-        total = 0
-        for marks in values:
-            total= total + marks
-        average =total /len(values)
-        print(name,"average:",average)
-each_students_average_marks(students)
+    total = 0
+    for mark in marks:
+         total= total + mark
+
+    return total /len(marks)
+        
+
+
+
+def check_student_pass_or_fail(average):
+
+    
+
+    if average >= 50:
+        result = "pass"
+    else:
+        result = "fail"
+
+
+
+def highest_average(students):
+    highest_average = 0
+    student_name=""
+    for name, marks in students.items():
+        average = each_students_average_marks(marks)
+
+        if average >highest_average:
+             highest_average = average  
+             student_name = name
+    return student_name, highest_average
+
+
+def lowest_average(students):
+    lowest_average = 999
+    student_name=""
+    for name, marks in students.items():
+        average = each_students_average_marks(marks)
+        if average < lowest_average:
+             lowest_average = average  
+             student_name = name
+    return student_name , lowest_average
+
+for name,marks in students.items():
+    average =each_students_average_marks(marks)
+    result =check_student_pass_or_fail(average)
+
+    print(
+        name,
+        "-Average",
+        round(average,2),
+        "-",
+        result
+    )
+
+
+
+highest_student, highest_mark = highest_average(students)
+lowest_student, lowest_mark = lowest_average(students)
+
+
+print("\nHighest Average:")
+print(highest_student, "-", round(highest_mark, 2))
+
+print("\nLowest Average:")
+print(lowest_student, "-", round(lowest_mark, 2))
